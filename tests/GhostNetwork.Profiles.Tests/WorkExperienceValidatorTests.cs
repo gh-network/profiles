6b3b0b256f643ef08c40a1fb8aa85be8a6a16fdc
Vlad <shcherbak.vlad10@gmail.com>
2020-10-31 22:59:49 +0200

    Fix namespace

--- tests/GhostNetwork.Profiles.Tests/WorkExperienceValidatorTests.cs
+++ tests/GhostNetwork.Profiles.Tests/WorkExperienceValidatorTests.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Linq;
-using GhostNetwork.Profiles.Domain;
+using GhostNetwork.Profiles.Domain.WorkExperiences;
 using NUnit.Framework;
 
 namespace GhostNetwork.Profiles.Tests
