3229efcb0c72c3d9480d55ec15589120b6016f0b
Vlad <shcherbak.vlad10@gmail.com>
2021-01-20 17:48:04 +0200

    add default security settings

---
+++ GhostNetwork.Profiles.Domain/SecuritySettings/DefaultSecuritySetting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GhostNetwork.Profiles.SecuritySettings
+{
+    public static class DefaultSecuritySetting
+    {
+        private static readonly SecuritySetting SecuritySetting = new SecuritySetting(default, default, Access.Everyone, Access.Everyone, new List<Guid>(), new List<Guid>());
+
+        public static SecuritySetting GetDefaultSecuritySetting()
+        {
+            return SecuritySetting;
+        }
+    }
+}
