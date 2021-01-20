21cf332331f40981a63947e7f4e37034344deb44
Vlad <shcherbak.vlad10@gmail.com>
2021-01-20 17:43:22 +0200

    add models for work with security settings

---
+++ GhostNetwork.Profiles.Domain/SecuritySettings/SecuritySetting.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GhostNetwork.Profiles.SecuritySettings
+{
+    public class SecuritySetting
+    {
+        public SecuritySetting(Guid id, Guid userId, Access accessToPosts, Access accessToFriends, List<Guid> certainUsersForPosts, List<Guid> certainUsersForFriends)
+        {
+            Id = id;
+            UserId = userId;
+            AccessToPosts = accessToPosts;
+            AccessToFriends = accessToFriends;
+            CertainUsersForPosts = certainUsersForPosts;
+            CertainUsersForFriends = certainUsersForFriends;
+        }
+
+        public Guid Id { get; }
+
+        public Guid UserId { get; }
+
+        public Access AccessToPosts { get; private set; }
+
+        public Access AccessToFriends { get; private set; }
+
+        public List<Guid> CertainUsersForPosts { get; private set; }
+
+        public List<Guid> CertainUsersForFriends { get; private set; }
+
+        public SecuritySetting Update(Access accessToPosts, Access accessToFriends, List<Guid> certainUsersForPosts, List<Guid> certainUsersForFriends)
+        {
+            AccessToPosts = accessToPosts;
+            AccessToFriends = accessToFriends;
+            CertainUsersForPosts = certainUsersForPosts;
+            CertainUsersForFriends = certainUsersForFriends;
+            return this;
+        }
+    }
+}
---
+++ Infrastructure/GhostNetwork.Profiles.MongoDb/SecuritySettingsEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace GhostNetwork.Profiles.MongoDb
+{
+    public class SecuritySettingsEntity
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+
+        [BsonElement("userId")]
+        public Guid UserId { get; set; }
+
+        [BsonElement("accessToPosts")]
+        public string AccessToPosts { get; set; }
+
+        [BsonElement("accessToFriends")]
+        public string AccessToFriends { get; set; }
+
+        [BsonElement("certainUsersForPosts")]
+        public List<Guid> CertainUsersForPosts { get; set; }
+
+        [BsonElement("certainUsersForFriends")]
+        public List<Guid> CertainUsersForFriends { get; set; }
+    }
+}
