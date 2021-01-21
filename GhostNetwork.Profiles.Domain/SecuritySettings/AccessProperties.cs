f347d0be3f7cb57a81137fe6156de8f6b2979dbf
Vlad <shcherbak.vlad10@gmail.com>
2021-01-21 15:04:06 +0200

    add additional model to store access type

---
+++ GhostNetwork.Profiles.Domain/SecuritySettings/AccessProperties.cs
@@ -0,0 +1,23 @@
+﻿namespace GhostNetwork.Profiles.SecuritySettings
+{
+    public class AccessProperties
+    {
+        public AccessProperties(Access accessToPosts, Access accessToFriends)
+        {
+            AccessToPosts = accessToPosts;
+            AccessToFriends = accessToFriends;
+        }
+
+        public Access AccessToPosts { get; private set; }
+
+        public Access AccessToFriends { get; private set; }
+
+        public AccessProperties Update(Access accessToPosts, Access accessToFriends)
+        {
+            AccessToPosts = accessToPosts;
+            AccessToFriends = accessToFriends;
+
+            return this;
+        }
+    }
+}
