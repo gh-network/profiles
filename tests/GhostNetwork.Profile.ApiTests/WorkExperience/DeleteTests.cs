b21f1d6a95902f84ec1c729f3faeaaa50aed3844
Alexander Khalieiev <Khalieiev1oleksandr@hneu.net>
2021-10-29 14:08:39 +0300

    add work experience api tests

---
+++ tests/GhostNetwork.Profile.ApiTests/WorkExperience/DeleteTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using GhostNetwork.Profiles;
+using GhostNetwork.Profiles.WorkExperiences;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using NUnit.Framework;
+
+namespace GhostNetwork.Profile.ApiTests.WorkExperience
+{
+    [TestFixture]
+    public class DeleteTests
+    {
+        [Test]
+        public async Task Delete_NoContent()
+        {
+            //Setup
+            var id = Guid.NewGuid();
+
+            var serviceMock = new Mock<IWorkExperienceService>();
+            var profileServiceMock = new Mock<IProfileService>();
+
+            serviceMock
+                .Setup(x => x.GetByIdAsync(id))
+                .ReturnsAsync(new Profiles.WorkExperiences.WorkExperience(id, It.IsAny<string>(), It.IsAny<string>(),
+                    DateTimeOffset.Now, DateTimeOffset.Now, Guid.NewGuid()));
+            
+            var client = TestServerHelper.New(collection =>
+            {
+                collection.AddScoped(_ => serviceMock.Object);
+                collection.AddScoped(_ => profileServiceMock.Object);
+            });
+            
+            //Act
+            var response = await client.DeleteAsync($"/WorkExperiences/{id}");
+            
+            //Assert
+            Assert.AreEqual(HttpStatusCode.NoContent, response.StatusCode);
+        }
+        
+        [Test]
+        public async Task Delete_NotFound()
+        {
+            //Setup
+            var id = Guid.NewGuid();
+
+            var serviceMock = new Mock<IWorkExperienceService>();
+            var profileServiceMock = new Mock<IProfileService>();
+
+            serviceMock
+                .Setup(x => x.GetByIdAsync(id))
+                .ReturnsAsync(default(Profiles.WorkExperiences.WorkExperience));
+            
+            var client = TestServerHelper.New(collection =>
+            {
+                collection.AddScoped(_ => serviceMock.Object);
+                collection.AddScoped(_ => profileServiceMock.Object);
+            });
+            
+            //Act
+            var response = await client.DeleteAsync($"/WorkExperiences/{id}");
+            
+            //Assert
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        }
+    }
+}
---
+++ tests/GhostNetwork.Profile.ApiTests/WorkExperience/UpdateTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Domain;
+using GhostNetwork.Profiles;
+using GhostNetwork.Profiles.Api.Models;
+using GhostNetwork.Profiles.WorkExperiences;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using NUnit.Framework;
+
+namespace GhostNetwork.Profile.ApiTests.WorkExperience
+{
+    [TestFixture]
+    public class UpdateTests
+    {
+        [Test]
+        public async Task Update_NoContent()
+        {
+            //Setup
+            var id = Guid.NewGuid();
+
+            var model = new WorkExperienceUpdateViewModel
+            {
+                CompanyName = "Company",
+                Description = "Desc",
+                StartWork = DateTimeOffset.Now,
+                FinishWork = DateTimeOffset.Now.AddDays(1)
+            };
+
+            var profileServiceMock = new Mock<IProfileService>();
+            var experienceServiceMock = new Mock<IWorkExperienceService>();
+
+            experienceServiceMock
+                .Setup(x => x.UpdateAsync(id, model.CompanyName, model.Description, model.StartWork, model.FinishWork))
+                .ReturnsAsync(DomainResult.Success);
+            
+            var client = TestServerHelper.New(collection =>
+            {
+                collection.AddScoped(_ => experienceServiceMock.Object);
+                collection.AddScoped(_ => profileServiceMock.Object);
+            });
+            
+            //Act
+            var response = await client.PutAsync($"/WorkExperiences/{id}", model.AsJsonContent());
+            
+            //Assert
+            Assert.AreEqual(HttpStatusCode.NoContent, response.StatusCode);
+        }
+        
+        [Test]
+        public async Task Update_NotFound()
+        {
+            //Setup
+            var id = Guid.NewGuid();
+
+            var model = new WorkExperienceUpdateViewModel
+            {
+                CompanyName = "Company",
+                Description = "Desc",
+                StartWork = DateTimeOffset.Now,
+                FinishWork = DateTimeOffset.Now.AddDays(1)
+            };
+
+            var profileServiceMock = new Mock<IProfileService>();
+            var experienceServiceMock = new Mock<IWorkExperienceService>();
+
+            experienceServiceMock
+                .Setup(x => x.UpdateAsync(id, model.CompanyName, model.Description, model.StartWork, model.FinishWork))
+                .ReturnsAsync(DomainResult.Error("SomeError"));
+            
+            var client = TestServerHelper.New(collection =>
+            {
+                collection.AddScoped(_ => experienceServiceMock.Object);
+                collection.AddScoped(_ => profileServiceMock.Object);
+            });
+            
+            //Act
+            var response = await client.PutAsync($"/WorkExperiences/{id}", model.AsJsonContent());
+            
+            //Assert
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+    }
+}
