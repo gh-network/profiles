a0139f8042d38ae6656afb8f28894c79a1690363
Alexander Khalieiev <Khalieiev1oleksandr@hneu.net>
2021-10-29 11:13:50 +0300

    add securitySetting api tests

---
+++ tests/GhostNetwork.Profile.ApiTests/SecuritySetting/GetTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using GhostNetwork.Profiles.SecuritySettings;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using NUnit.Framework;
+
+namespace GhostNetwork.Profile.ApiTests.SecuritySetting
+{
+    [TestFixture]
+    public class GetTests
+    {
+        [Test]
+        public async Task GetByUserId_Ok()
+        {
+            //Setup
+            var userId = Guid.NewGuid();
+
+            var securitySetting = new Profiles.SecuritySettings.SecuritySetting(
+                userId,
+                new SecuritySettingsSection(Access.Everyone, new List<Guid>()),
+                new SecuritySettingsSection(Access.Everyone, new List<Guid>()));
+
+            var serviceMock = new Mock<ISecuritySettingService>();
+
+            serviceMock
+                .Setup(x => x.GetByUserIdAsync(userId))
+                .ReturnsAsync(securitySetting);
+
+            var client = TestServerHelper.New(collection =>
+            {
+                collection.AddScoped(_ => serviceMock.Object);
+            });
+            
+            //Act
+            var response = await client.GetAsync($"/profiles/{userId}/security-settings");
+            
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+        
+        [Test]
+        public async Task GetByUserId_NotFound()
+        {
+            //Setup
+            var userId = Guid.NewGuid();
+
+            var serviceMock = new Mock<ISecuritySettingService>();
+
+            serviceMock
+                .Setup(x => x.GetByUserIdAsync(userId))
+                .ReturnsAsync(default(Profiles.SecuritySettings.SecuritySetting));
+
+            var client = TestServerHelper.New(collection =>
+            {
+                collection.AddScoped(_ => serviceMock.Object);
+            });
+            
+            //Act
+            var response = await client.GetAsync($"/profiles/{userId}/security-settings");
+            
+            //Assert
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        }
+    }
+}
