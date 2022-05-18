76a8bf06c2aea381401ee69fdbfeb1fe8d5aa649
Bohdan Kolomeiets <38009812+bod9i@users.noreply.github.com>
2022-05-18 12:35:40 +0300

    create inputModel for selecting many settings

---
+++ GhostNetwork.Profiles.Api/Models/SecuritySettingsByManyIdsViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GhostNetwork.Profiles.Api.Models;
+
+public class SecuritySettingsByManyIdsViewModel
+{
+    public IEnumerable<Guid> UserIds { get; set; }
+}
