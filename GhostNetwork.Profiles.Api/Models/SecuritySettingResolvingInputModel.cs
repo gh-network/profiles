90e632401c47baa050e2a70b934f84b989f3670f
Bohdan Kolomeiets <38009812+bod9i@users.noreply.github.com>
2022-06-17 16:56:16 +0300

    create separate file for input model

---
+++ GhostNetwork.Profiles.Api/Models/SecuritySettingResolvingInputModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace GhostNetwork.Profiles.Api.Models
+{
+    public class SecuritySettingResolvingInputModel
+    {
+        public Guid ToUserId { get; set; }
+
+        public string SectionName { get; set; }
+    }
+}
